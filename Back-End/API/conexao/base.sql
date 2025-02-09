853d088a7bca12c55aa425cfa167848e1021defa
Fellipe Marques <104071374+FellipeMSC@users.noreply.github.com>
2025-02-09 18:45:02 -0300

    Update base.sql

--- Back-End/API/conexao/base.sql
+++ Back-End/API/conexao/base.sql
@@ -1,13 +1,12 @@
-CREATE DATABASE  IF NOT EXISTS `dewi2024`;
+CREATE DATABASE  IF NOT EXISTS `ecommerce`;
 
-USE `dewi2024`;
+USE `ecommerce`;
 
-DROP TABLE IF EXISTS `cartas`;
+DROP TABLE IF EXISTS `cliente`;
 
-CREATE TABLE `cartas` (
+CREATE TABLE `cliente` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(128) DEFAULT NULL,
-  `descricao` varchar(512) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
 
@@ -20,3 +19,30 @@ CREATE TABLE `usuarios` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
 
+DROP TABLE IF EXISTS `produto`;
+
+CREATE TABLE `produto` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(128) DEFAULT NULL,
+  `preço` varchar(128) DEFAULT NULL,
+  `quantidade` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+
+DROP TABLE IF EXISTS `funcionario`;
+
+CREATE TABLE `funcionario` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+
+DROP TABLE IF EXISTS `fornecedor`;
+usuarios
+CREATE TABLE `fornecedor` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(128) DEFAULT NULL,
+  `endereço` varchar(128) DEFAULT NULL,
+  `telefone` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
